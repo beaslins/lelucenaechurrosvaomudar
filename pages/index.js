985c95f59aa5fab42bb3d013dcfa9a47ddb5ec2d
Beatriz Santos Lins <beatriz@worksmidia.com.br>
2021-11-07 17:15:56 -0300

    final version 0711

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,13 @@ import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+
+  if (typeof window !== "undefined") {
+    var video = document.querySelector('video');
+    video.muted = true;
+    video.play()
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -18,6 +25,11 @@ export default function Home() {
 
       <main>
         <header className={styles.bgheader}>
+
+          <video loop="true" autoplay="autoplay" className={styles.myVideo}>
+            <source src="/leevi.mp4" type="video/mp4" />
+          </video>
+          
           <img src="/logo.png" alt="" />
         </header>
 
@@ -26,7 +38,7 @@ export default function Home() {
           <div className={styles.item}>
               <Image src="/pano.png" alt="" width={300} height={400} layout='responsive'/>
               <p className={styles.itemName}>Pano de prato</p>
-              <p className={styles.itemPrice}>R$ 20,00</p>
+              <p className={styles.itemPrice}>R$ 10,00</p>
               <Link href="https://pag.ae/7XGxiW1G7">
                 <a target="_blank">Comprar</a>
               </Link>
@@ -35,24 +47,167 @@ export default function Home() {
               <Image src="/balde.png" alt="" width={300} height={400} layout='responsive'/>
               <p className={styles.itemName}>Balde</p>
               <p className={styles.itemPrice}>R$ 20,00</p>
-              <Link href="">
-                <a>Comprar</a>
+              <Link href="https://pag.ae/7XGxEiUB1">
+                <a target="_blank">Comprar</a>
               </Link>
             </div>
             <div className={styles.item}>
               <Image src="/baldegamer.png" alt="" width={300} height={400} layout='responsive'/>
-              <p className={styles.itemName}>Balde</p>
-              <p className={styles.itemPrice}>R$ 20,00</p>
-              <Link href="">
-                <a>Comprar</a>
+              <p className={styles.itemName}>Balde Gamer</p>
+              <p className={styles.itemPrice}>R$ 30,00</p>
+              <Link href="https://pag.ae/7XGxEHvBa">
+                <a target="_blank">Comprar</a>
               </Link>
             </div>
             <div className={styles.item}>
               <Image src="/casamento.png" alt="" width={300} height={400} layout='responsive'/>
               <p className={styles.itemName}>Fazer o Vinicius pedir a Leticia em noivado</p>
-              <p className={styles.itemPrice}>R$ 20,00</p>
-              <Link href="">
-                <a>Comprar</a>
+              <p className={styles.itemPrice}>R$ 44,00</p>
+              <Link href="https://pag.ae/7XGxFBUNN">
+                <a target="_blank">Comprar</a>
+              </Link>
+            </div>
+        </div>
+
+
+        <div className={styles.list}>
+          <div className={styles.item}>
+              <Image src="/panela.png" alt="" width={300} height={400} layout='responsive'/>
+              <p className={styles.itemName}>Panela</p>
+              <p className={styles.itemPrice}>R$ 50,00</p>
+              <Link href="https://pag.ae/7XGxHkiHN">
+                <a target="_blank">Comprar</a>
+              </Link>
+            </div>
+            <div className={styles.item}>
+              <Image src="/copo.png" alt="" width={300} height={400} layout='responsive'/>
+              <p className={styles.itemName}>Conjunto de Copos</p>
+              <p className={styles.itemPrice}>R$ 60,00</p>
+              <Link href="https://pag.ae/7XGxHUHNG">
+                <a target="_blank">Comprar</a>
+              </Link>
+            </div>
+            <div className={styles.item}>
+              <Image src="/ashe.png" alt="" width={300} height={400} layout='responsive'/>
+              <p className={styles.itemName}>Cama para Ashe</p>
+              <p className={styles.itemPrice}>R$ 80,00</p>
+              <Link href="https://pag.ae/7XGxJh_TK">
+                <a target="_blank">Comprar</a>
+              </Link>
+            </div>
+            <div className={styles.item}>
+              <Image src="/tacas.png" alt="" width={300} height={400} layout='responsive'/>
+              <p className={styles.itemName}>Conjunto de Taças</p>
+              <p className={styles.itemPrice}>R$ 100,00</p>
+              <Link href="https://pag.ae/7XGxJHsVs">
+                <a target="_blank">Comprar</a>
+              </Link>
+            </div>
+        </div>
+
+        <div className={styles.list}>
+          <div className={styles.item}>
+              <Image src="/almofada.png" alt="" width={300} height={400} layout='responsive'/>
+              <p className={styles.itemName}>Almofadas</p>
+              <p className={styles.itemPrice}>R$ 120,00</p>
+              <Link href="https://pag.ae/7XGxKCVB7">
+                <a target="_blank">Comprar</a>
+              </Link>
+            </div>
+            <div className={styles.item}>
+              <Image src="/cobertor.png" alt="" width={300} height={400} layout='responsive'/>
+              <p className={styles.itemName}>Cobertor</p>
+              <p className={styles.itemPrice}>R$ 150,00</p>
+              <Link href="https://pag.ae/7XGxLwYHN">
+                <a target="_blank">Comprar</a>
+              </Link>
+            </div>
+            <div className={styles.item}>
+              <Image src="/escada.png" alt="" width={300} height={400} layout='responsive'/>
+              <p className={styles.itemName}>Escada</p>
+              <p className={styles.itemPrice}>R$ 180,00</p>
+              <Link href="https://pag.ae/7XGxLYgw1">
+                <a target="_blank">Comprar</a>
+              </Link>
+            </div>
+            <div className={styles.item}>
+              <Image src="/impressora.png" alt="" width={300} height={400} layout='responsive'/>
+              <p className={styles.itemName}>Impressora</p>
+              <p className={styles.itemPrice}>R$ 200,00</p>
+              <Link href="https://pag.ae/7XGxMGf-s">
+                <a target="_blank">Comprar</a>
+              </Link>
+            </div>
+        </div>
+
+
+
+        <div className={styles.list}>
+          <div className={styles.item}>
+              <Image src="/privada.png" alt="" width={300} height={400} layout='responsive'/>
+              <p className={styles.itemName}>Privada Gamer</p>
+              <p className={styles.itemPrice}>R$ 250,00</p>
+              <Link href="https://pag.ae/7XGxPjFxN">
+                <a target="_blank">Comprar</a>
+              </Link>
+            </div>
+            <div className={styles.item}>
+              <Image src="/pratps.png" alt="" width={300} height={400} layout='responsive'/>
+              <p className={styles.itemName}>Conjunto de Pratos</p>
+              <p className={styles.itemPrice}>R$ 300,00</p>
+              <Link href="https://pag.ae/7XGxQitQs">
+                <a target="_blank">Comprar</a>
+              </Link>
+            </div>
+            <div className={styles.item}>
+              <Image src="/forno.png" alt="" width={300} height={400} layout='responsive'/>
+              <p className={styles.itemName}>Forno</p>
+              <p className={styles.itemPrice}>R$ 400,00</p>
+              <Link href="https://pag.ae/7XGxQXG9v">
+                <a target="_blank">Comprar</a>
+              </Link>
+            </div>
+            <div className={styles.item}>
+              <Image src="/pc.png" alt="" width={300} height={400} layout='responsive'/>
+              <p className={styles.itemName}>Parcela PC Gamer</p>
+              <p className={styles.itemPrice}>R$ 500,00</p>
+              <Link href="https://pag.ae/7XGxRegm1">
+                <a target="_blank">Comprar</a>
+              </Link>
+            </div>
+        </div>
+
+        <div className={styles.list}>
+          <div className={styles.item}>
+              <Image src="/cadeira.png" alt="" width={300} height={400} layout='responsive'/>
+              <p className={styles.itemName}>Cadeira Gamer</p>
+              <p className={styles.itemPrice}>R$ 600,00</p>
+              <Link href="https://pag.ae/7XGxRN2Qs">
+                <a target="_blank">Comprar</a>
+              </Link>
+            </div>
+            <div className={styles.item}>
+              <Image src="/cama.png" alt="" width={300} height={400} layout='responsive'/>
+              <p className={styles.itemName}>Cama</p>
+              <p className={styles.itemPrice}>R$ 700,00</p>
+              <Link href="https://pag.ae/7XGxShEGa">
+                <a target="_blank">Comprar</a>
+              </Link>
+            </div>
+            <div className={styles.item}>
+              <Image src="/carro.png" alt="" width={300} height={400} layout='responsive'/>
+              <p className={styles.itemName}>Parcela do Carro</p>
+              <p className={styles.itemPrice}>R$ 800,00</p>
+              <Link href="https://pag.ae/7XGxSDjjs">
+                <a target="_blank">Comprar</a>
+              </Link>
+            </div>
+            <div className={styles.item}>
+              <Image src="/barba.png" alt="" width={300} height={400} layout='responsive'/>
+              <p className={styles.itemName}>Tirar a barba do Vinicius</p>
+              <p className={styles.itemPrice}>R$ 1000,00</p>
+              <Link href="https://pag.ae/7XGxTsJVm">
+                <a target="_blank">Comprar</a>
               </Link>
             </div>
         </div>
